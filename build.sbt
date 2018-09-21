0c170ce967bb77e92ee34f980e0479213f4e7145
Mitchell Skaggs <skaggsm333@gmail.com>
2018-09-20 22:06:28 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "canvas-grade-data-uploader"
+
+version := "0.1.0"
+
+scalaVersion := "2.12.6"
+
+enablePlugins(JavaAppPackaging)
---
+++ src/main/scala/com/skaggsm/gradeupload/Main.scala
@@ -0,0 +1,10 @@
+package com.skaggsm.gradeupload
+
+/**
+  * Created by Mitchell Skaggs on 9/20/2018.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
